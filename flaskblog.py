a0b314168cba4bd2207fdbd971db0da903c08d49
Matt Nannetti <mattnannetti@gmail.com>
2020-11-30 15:03:27 +0100

    adding forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '54cb0a1257b1585952f7ea15492dc6fa'
 
 posts = [
     {
@@ -33,13 +36,16 @@ def about():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm
+    return render_template('login.html', tilte='Login', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
-
-
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Accounted created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
