b90e633dd17facec811d11e5da303abc37bc4f55
Matt Nannetti <mattnannetti@gmail.com>
2020-12-03 15:11:44 +0100

    profile page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '54cb0a1257b1585952f7ea15492dc6fa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -11,5 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
