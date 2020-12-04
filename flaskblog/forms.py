5af6a1f2a9d5882f59044131868abdc7a48d8e00
Matt Nannetti <mattnannetti@gmail.com>
2020-12-04 14:40:39 +0100

    profile pic update enabled

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -39,11 +39,11 @@ class UpdateAccountForm(FlaskForm):
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is already taken.')
+            if user:
+                raise ValidationError('That username is already taken.')
     
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is already taken.')
+            if user:
+                raise ValidationError('That email is already taken.')
